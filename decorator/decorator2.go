e557670f7e0b9ac6c76c7f0709b2425ea62c4b6c
真人 <weizhen.wang@duitang.com>
2017-06-03 15:40:19 +0800

    add decorator

---
+++ decorator/decorator2.go
@@ -0,0 +1,21 @@
+// See http://coolshell.cn/articles/17929.html
+package main
+ 
+import "fmt"
+ 
+func decorator(f func(s string)) func(s string) {
+ 
+        return func(s string) {
+                fmt.Println("Started")
+                f(s)
+                fmt.Println("Done")
+        }
+}
+ 
+func Hello(s string) {
+        fmt.Println(s)
+}
+ 
+func main() {
+        decorator(Hello)("Hello, World!")
+}
